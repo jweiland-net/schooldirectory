78af263eb9841f137fae724998e027947b79a13c
Stefan Froemken <froemken@gmail.com>
2018-11-20 15:48:28 +0100

    Make use of Maps2 Registry

--- Configuration/TCA/Overrides/tx_schooldirectory_domain_model_school.php
+++ Configuration/TCA/Overrides/tx_schooldirectory_domain_model_school.php
@@ -6,3 +6,25 @@ defined('TYPO3_MODE') or die();
     'tx_schooldirectory_domain_model_school',
     'category'
 );
+
+// Add tx_maps2_uid column to school table
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('maps2')) {
+    \JWeiland\Maps2\Tca\Maps2Registry::getInstance()->add(
+        'schooldirectory',
+        'tx_schooldirectory_domain_model_school',
+        [
+            'addressColumns' => ['street', 'house_number', 'zip', 'city'],
+            'defaultCountry' => 'Deutschland',
+            'defaultStoragePid' => [
+                'extKey' => 'schooldirectory',
+                'property' => 'poiCollectionPid'
+            ],
+            'synchronizeColumns' => [
+                [
+                    'foreignColumnName' => 'title',
+                    'poiCollectionColumnName' => 'title'
+                ]
+            ]
+        ]
+    );
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$boot = function() {
+call_user_func(function () {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'JWeiland.schooldirectory',
         'List',
@@ -29,9 +29,6 @@ $boot = function() {
             'District' => 'list'
         ]
     );
-    // use hook to automatically add a map record to current yellow page
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \JWeiland\Schooldirectory\Tca\CreateMap::class;
-
     /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
     $iconRegistry->registerIcon(
@@ -42,13 +39,11 @@ $boot = function() {
 
     // add schooldirectory plugin to new element wizard
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:schooldirectory/Configuration/TSconfig/ContentElementWizard.txt">');
-};
-$boot();
-unset($boot);
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['schooldirectory_selector_renderTypeAction']
-    = \JWeiland\Schooldirectory\Ajax\Selector::class . '::renderTypeAction';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['schooldirectory_selector_renderCareFormAction']
-    = \JWeiland\Schooldirectory\Ajax\Selector::class . '::renderCareFormAction';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['schooldirectory_selector_renderProfileAction']
-    = \JWeiland\Schooldirectory\Ajax\Selector::class . '::renderProfileAction';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['schooldirectory_selector_renderTypeAction']
+        = \JWeiland\Schooldirectory\Ajax\Selector::class . '::renderTypeAction';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['schooldirectory_selector_renderCareFormAction']
+        = \JWeiland\Schooldirectory\Ajax\Selector::class . '::renderCareFormAction';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['schooldirectory_selector_renderProfileAction']
+        = \JWeiland\Schooldirectory\Ajax\Selector::class . '::renderProfileAction';
+});
