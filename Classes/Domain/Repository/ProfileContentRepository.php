856a46e61f051e5006e5fba1d994c42ebd8b871b
Stefan Froemken <froemken@gmail.com>
2022-01-07 17:26:21 +0100

    Update lines mentioned by php-cs-fixer

--- Classes/Domain/Repository/ProfileContentRepository.php
+++ Classes/Domain/Repository/ProfileContentRepository.php
@@ -11,12 +11,6 @@ declare(strict_types=1);
 
 namespace JWeiland\Schooldirectory\Domain\Repository;
 
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Persistence\Generic\Query;
-use TYPO3\CMS\Extbase\Persistence\QueryInterface;
-use TYPO3\CMS\Extbase\Persistence\Repository;
-
 /**
  * Repository to find profile content by a given school type and careform
  */
