b38f04b84330d6e3594f7c18dc9728b30fb3f3f4
Stefan Froemken <froemken@gmail.com>
2023-03-13 14:18:26 +0100

    Remove unused import from SchoolRepository

--- Classes/Domain/Repository/SchoolRepository.php
+++ Classes/Domain/Repository/SchoolRepository.php
@@ -13,11 +13,9 @@ namespace JWeiland\Schooldirectory\Domain\Repository;
 
 use JWeiland\Schooldirectory\Domain\Repository\Traits\QueryBuilderTrait;
 use TYPO3\CMS\Core\Database\Connection;
-use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Persistence\Generic\Query;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
