75eae9055408ed12f05c024255cba43a0a5a3a69
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-06-18 12:29:00 +0200

    [TASK] Migration wizard for list_type to CType

---
+++ Classes/Updates/PluginToContentElementUpdate.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/schooldirectory.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Schooldirectory\Update;
+
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+use TYPO3\CMS\Install\Updates\AbstractListTypeToCTypeUpdate;
+
+/**
+ * With TYPO3 13 all plugins have to be declared as content elements (CType) insteadof "list_type"
+ */
+#[UpgradeWizard('schooldirectory_migratePluginsToContentElementsUpdate')]
+class PluginToContentElementUpdate extends AbstractListTypeToCTypeUpdate
+{
+    protected function getListTypeToCTypeMapping(): array
+    {
+        return [
+            'schooldirectory_list' => 'schooldirectory_list',
+            'schooldirectory_search' => 'schooldirectory_search',
+        ];
+    }
+
+    public function getTitle(): string
+    {
+        return 'EXT:schooldirectory - Migrate plugins to Content Elements';
+    }
+
+    public function getDescription(): string
+    {
+        return 'The modern way to register plugins for TYPO3 is to register them as content element types. ' .
+            'Running this wizard will migrate all schooldirectory plugins to content element (CType)';
+    }
+}
